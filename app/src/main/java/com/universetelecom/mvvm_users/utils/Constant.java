11bb1434e4374bebba481c1f403ed013683a4e8c
Ahmad Shubita <ah.shubita@gmail.com>
2017-08-04 18:38:48 +0300

    d

--- app/src/main/java/com/universetelecom/mvvm_users/utils/Constant.java
+++ app/src/main/java/com/universetelecom/mvvm_users/utils/Constant.java
@@ -8,6 +8,6 @@ public interface Constant {
 
     public final  String BASE_URL = "http://api.randomuser.me/";
     public final  String RANDOM_USER_URL = "http://api.randomuser.me/?results=10&nat=en";
-    public final  String PROJECT_URL = "";
+    public final  String PROJECT_URL = "https://github.com/AhmadShubita/MVVM-with-RXJava-and-Retrofit-Users-Generated-Example-";
 
 }
